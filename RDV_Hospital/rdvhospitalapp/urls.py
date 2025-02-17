d77e5617bf3d8f94747fa22a939777b7b5c37299
TFOA2 <fokouongsirus11@gmail.com>
2025-02-17 10:17:55 +0100

    mise en place des interfaces patient

--- RDV_Hospital/rdvhospitalapp/urls.py
+++ RDV_Hospital/rdvhospitalapp/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from .views import index
+from .views import indexAccueil,dashboard,connexion,inscription,choixUtilisateur,ajoutPrendreRendezVous,consultation,urgence,profil
 
 urlpatterns = [
-    path('',index,name="accueil")
+    path('',indexAccueil,name="accueil"),
+    path('dashboard/',dashboard,name="dashboard"),
+    path('choixUtilisateur/',choixUtilisateur),
+    path('connexion/',connexion,name="dashboardPatient"),
+    path('inscription/<int:type>/',inscription,name="inscription"),
+    path('prendre_rendez-vous/',ajoutPrendreRendezVous,name="store"),
+    path('consultation/',consultation),
+    path('urgence/',urgence),
+    path('profil/',profil),
     
 ]
--- RDV_Hospital/rdvhospitalapp/views.py
+++ RDV_Hospital/rdvhospitalapp/views.py
@@ -3,7 +3,49 @@ from django.http import HttpResponse
 from django.template import loader
 
 
-def index(request):
-    template = loader.get_template("patient/index.html")
-    return HttpResponse(template.render())
+def indexAccueil(request):
+    template = loader.get_template("index.html")
+    name = {
+        'firstname': ["TFOA2", "Junior", "Arnold", "Arthur", "Alfred"],
+        'note':{
+            'math':15,
+            'Physique':14
+            }
+        }
+    
+    return HttpResponse(template.render(name,request))
 
+def dashboard(request):
+    
+    return render(request,'patient/index.html')
+
+def choixUtilisateur(request):
+    
+    return render(request,'userChoice.html')
+
+def connexion(request):
+    
+    return render(request, "connexion.html")
+
+def inscription(request,type):
+    context = {
+        'type':type
+    }
+    
+    return render(request, "inscription.html",context)
+    
+def ajoutPrendreRendezVous(request):
+    
+    return render(request, "patient/ajout.html")
+
+def consultation(request):
+    
+    return render(request,"patient/consultation.html")
+
+def urgence(request):
+    
+    return render(request,"patient/urgence.html")
+
+def profil(request):
+    
+    return render(request,"patient/profil.html")
